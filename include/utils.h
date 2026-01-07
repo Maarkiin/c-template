6be51eef35e30652976188fa5ea22486aefe5f03
Maarkiin <137499329+Maarkiin@users.noreply.github.com>
2026-01-07 04:53:48 +0000

    Update utils.h

--- include/utils.h
+++ include/utils.h
@@ -10,36 +10,66 @@ static inline void printargs(int argc, char** argv, char* title)
     if (title) titlelen = strlen(title);
     const char t = 205; /*═*/
     const char s = 186; /*║*/
-    int max = 0;
+    int max = 0, m;
     for (int i = 0; i < argc; ++i)
     {
         int slen = strlen(argv[i]);
         if (slen > max) max = slen; 
     }
 
-    /*                 header                 */
-    int m = (max-titlelen)/2;
-    printf("%c", 201); /*╔*/
-    for (int i = 0; i < m; i++) printf("%c", t);
-    printf("%s", title);
-    if (titlelen % 2 == 0) m++;
-    for (int i = 0; i < m; i++) printf("%c", t);
-    printf("%c", 187); /*╗*/
-    /*                                        */
-
-    /*                contents                */
-    printf("\n");
-    for (int i = 0; i < argc; i++)
+    if ((max % 2) == 0)
     {
-        m = (max - strlen(argv[i])) / 2;
-        printf("%c", s);
-        for (int i = 0; i < m; i++) printf(" ");
-        printf("%s", argv[i]);
-        if (strlen(argv[i]) % 2 == 0) m++;
-        for (int i = 0; i < m; i++) printf(" ");
-        printf("%c\n", s);
+        /*                 header                 */
+        m = (max-titlelen)/2;
+        printf("%c", 201); /*╔*/
+        for (int i = 0; i < m; i++) printf("%c", t);
+        printf("%s", title);
+        if (titlelen % 2 != 0) m++;
+        for (int i = 0; i < m; i++) printf("%c", t);
+        printf("%c", 187); /*╗*/
+        /*                                        */
+
+        /*                contents                */
+        printf("\n");
+        for (int i = 0; i < argc; i++)
+        {
+            m = (max - strlen(argv[i])) / 2;
+            printf("%c", s);
+            for (int i = 0; i < m; i++) printf(" ");
+            printf("%s", argv[i]);
+            if (strlen(argv[i]) % 2 != 0) m++;
+            for (int i = 0; i < m; i++) printf(" ");
+            printf("%c\n", s);
+        }
+        /*                                        */
+    } else
+    {
+        /*                 header                 */
+        m = (max-titlelen)/2;
+        printf("%c", 201); /*╔*/
+        for (int i = 0; i < m; i++) printf("%c", t);
+        printf("%s", title);
+        if (titlelen % 2 == 0) m++;
+        for (int i = 0; i < m; i++) printf("%c", t);
+        printf("%c", 187); /*╗*/
+        /*                                        */
+
+        /*                contents                */
+        printf("\n");
+        for (int i = 0; i < argc; i++)
+        {
+            m = (max - strlen(argv[i])) / 2;
+            printf("%c", s);
+            for (int i = 0; i < m; i++) printf(" ");
+            printf("%s", argv[i]);
+            if (strlen(argv[i]) % 2 == 0) m++;
+            for (int i = 0; i < m; i++) printf(" ");
+            printf("%c\n", s);
+        }
+        /*                                        */
     }
-    /*                                        */
+
+    
 
     /*                 footer                 */
     m = max;
@@ -49,5 +79,6 @@ static inline void printargs(int argc, char** argv, char* title)
     /*                                        */
     printf("\n");
 }
+
 
 #endif
