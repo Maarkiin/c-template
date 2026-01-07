303c05d3207ebbfc4f71e19a9f68379d1bd57099
Maarkiin <137499329+Maarkiin@users.noreply.github.com>
2026-01-07 04:25:07 +0000

    .

--- include/utils.h
+++ include/utils.h
@@ -2,11 +2,52 @@
 #define utilsH
 
 #define UNUSED __attribute__((unused))
+#define uint unsigned int
 
-#include <stdint.h>
-#define uint8 uint8_t
-#define uint16 uint16_t
-#define uint32 uint32_t
-#define uint64 uint64_t
+static inline void printargs(int argc, char** argv, char* title)
+{
+    int titlelen = 0;
+    if (title) titlelen = strlen(title);
+    const char t = 205; /*═*/
+    const char s = 186; /*║*/
+    int max = 0;
+    for (int i = 0; i < argc; ++i)
+    {
+        int slen = strlen(argv[i]);
+        if (slen > max) max = slen; 
+    }
+
+    /*                 header                 */
+    int m = (max-titlelen)/2;
+    printf("%c", 201); /*╔*/
+    for (int i = 0; i < m; i++) printf("%c", t);
+    printf("%s", title);
+    if (titlelen % 2 == 0) m++;
+    for (int i = 0; i < m; i++) printf("%c", t);
+    printf("%c", 187); /*╗*/
+    /*                                        */
+
+    /*                contents                */
+    printf("\n");
+    for (int i = 0; i < argc; i++)
+    {
+        m = (max - strlen(argv[i])) / 2;
+        printf("%c", s);
+        for (int i = 0; i < m; i++) printf(" ");
+        printf("%s", argv[i]);
+        if (strlen(argv[i]) % 2 == 0) m++;
+        for (int i = 0; i < m; i++) printf(" ");
+        printf("%c\n", s);
+    }
+    /*                                        */
+
+    /*                 footer                 */
+    m = max;
+    printf("%c", 200); /*╚*/
+    for (int i = 0; i < m; i++) printf("%c", t);
+    printf("%c", 188); /*╝*/
+    /*                                        */
+    printf("\n");
+}
 
 #endif
